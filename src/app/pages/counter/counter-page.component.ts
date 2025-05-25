533a433b65386e147c16415fc2756d9495fb4cd8
milton munoz <livemilton@hotmail.com>
2025-05-25 18:47:57 -0500

    angular - task separate template from logic in a html

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,17 +1,28 @@
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    template: `
-    <h1>Counter: {{ counter }}</h1>
-    <button (click)="increaseBy(1)">+1</button>
-    <!-- <h2>CounterComponent Page</h2> -->
-    `,
+    //templateUrl
+    imports: [RouterOutlet],
+    templateUrl: './counter-page.component.html',
+    styles: `
+    button{
+        padding: 5px;
+        margin: 5px 10px;
+        width: 75px;
+    }
+    `
 })
 export class CounterPageComponent{
     counter= 15;
 
     increaseBy(value:number){
         this.counter += value;
+    }
+    decreaseBy(value:number){
+        this.counter -=value;
+    }
+    resetCounter(){
+        this.counter=15;
     }
 }
