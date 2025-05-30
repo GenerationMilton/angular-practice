25d191875d0a104476ac4e815375fd7f0ea1b140
Milton Munoz <livemilton@gmail.com>
2025-05-29 22:10:59 -0500

    angular - services

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,7 +1,8 @@
 
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add.component";
+import { DragonballService } from '../../services/dragonball.service';
 
 interface Character {
     id:number;
@@ -17,23 +18,8 @@ interface Character {
 })
 
 export class DragonballSuperPageComponent {    
-    //input for character creation use a signal
-    name = signal('')
-    power = signal(0)
-
-    characters = signal<Character[]>([
-        { id:1 , name: 'Goku', power: 9001 },
-        { id:2 , name: 'Vegeta', power: 8000 },
-      
-    ]);
-
-    addCharacter(character: Character){
-        this.characters.update((list) => [ ...list, character]);
-       
-    }
+  
+    public dragonballService = inject(DragonballService);
     
-    resetFields() {
-        this.name.set('');
-        this.power.set(0);
-    }
+    
 }
